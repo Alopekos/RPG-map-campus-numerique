03f081f114d6d9cf887ac673d03e1d981e4ffa97
remi.legrand <remi.legrand@le-campus-numerique.fr>
2024-10-02 16:26:48 +0200

    Link to RPG website

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup></script>
 
 <template>
+  <nav>
+    <a href="http://172.22.114.110/"
+      >🧙‍♂️ Site Jdr
+      <span class="parenthesis">(Accessible seulement en local)</span></a
+    >
+  </nav>
   <router-view></router-view>
 </template>
 <style>
